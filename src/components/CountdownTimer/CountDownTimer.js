0d198e19f4a6326ea20455f031740098dc494e46
fjnelson <97076779+fjnelson@users.noreply.github.com>
2023-04-28 21:08:17 -0700

    added a few go-back-buttons

--- src/components/CountdownTimer/CountDownTimer.js
+++ src/components/CountdownTimer/CountDownTimer.js
@@ -1,17 +1,28 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 import TimerApp from "./Timer.js";
 import CountDown from "./CountDown.js";
+import Button from 'react-bootstrap/Button';
 
 const CountDownTimer = () => {
+    const navigate = useNavigate();
+
+    const goBack = () => {
+        navigate(-1);
+    };
+
     return (
-      <Container>
-        <div>
-        <TimerApp />
-        <CountDown />
-        </div>
-      </Container>
+        <Container>
+            <div>
+                <TimerApp />
+                <CountDown />
+            </div>
+            <div>
+                <Button variant="primary" onClick={goBack}>Go Back</Button>{' '}
+            </div>
+        </Container>
     );
-  };
-  
-  export default CountDownTimer;
+};
+
+export default CountDownTimer;
--- src/components/FeatureFunctionClass/FeatureFunctionClass.js
+++ src/components/FeatureFunctionClass/FeatureFunctionClass.js
@@ -1,74 +1,87 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import "../../App.css";
+import Button from 'react-bootstrap/Button';
+import { useNavigate } from "react-router-dom";
 
 const CardComponent = () => {
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1);
+  }
+
   return (
-    <div className="FFC-container">
-      <Card>
-        <Card.Img
-          className="FFC-card"
-          variant="top"
-          src={`https://source.unsplash.com/200x200/?vehicle&${Math.random()}`}
-        />
-        <Card.Body>
-          <Card.Title className="FFC-title">Vehicles</Card.Title>
-        </Card.Body>
-      </Card>
-      <Card>
-        <Card.Img
-          className="FFC-card"
-          variant="top"
-          src={`https://source.unsplash.com/200x200/?furniture&${Math.random()}`}
-        />
-        <Card.Body>
-          <Card.Title className="FFC-title">Furniture</Card.Title>
-        </Card.Body>
-      </Card>
+    <div>
+      <div className="FFC-container">
+        <Card>
+          <Card.Img
+            className="FFC-card"
+            variant="top"
+            src={`https://source.unsplash.com/200x200/?vehicle&${Math.random()}`}
+          />
+          <Card.Body>
+            <Card.Title className="FFC-title">Vehicles</Card.Title>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Img
+            className="FFC-card"
+            variant="top"
+            src={`https://source.unsplash.com/200x200/?furniture&${Math.random()}`}
+          />
+          <Card.Body>
+            <Card.Title className="FFC-title">Furniture</Card.Title>
+          </Card.Body>
+        </Card>
 
-      <Card>
-        <Card.Img
-          className="FFC-card"
-          variant="top"
-          src={`https://source.unsplash.com/200x200/?clothing&${Math.random()}`}
-        />
-        <Card.Body>
-          <Card.Title className="FFC-title">Clothing</Card.Title>
-        </Card.Body>
-      </Card>
+        <Card>
+          <Card.Img
+            className="FFC-card"
+            variant="top"
+            src={`https://source.unsplash.com/200x200/?clothing&${Math.random()}`}
+          />
+          <Card.Body>
+            <Card.Title className="FFC-title">Clothing</Card.Title>
+          </Card.Body>
+        </Card>
 
-      <Card>
-        <Card.Img
-          className="FFC-card"
-          variant="top"
-          src={`https://source.unsplash.com/200x200/?Food&${Math.random()}`}
-        />
-        <Card.Body>
-          <Card.Title className="FFC-title">Food</Card.Title>
-        </Card.Body>
-      </Card>
+        <Card>
+          <Card.Img
+            className="FFC-card"
+            variant="top"
+            src={`https://source.unsplash.com/200x200/?Food&${Math.random()}`}
+          />
+          <Card.Body>
+            <Card.Title className="FFC-title">Food</Card.Title>
+          </Card.Body>
+        </Card>
 
-      <Card>
-        <Card.Img
-          className="FFC-card"
-          variant="top"
-          src={`https://source.unsplash.com/200x200/?animal&${Math.random()}`}
-        />
-        <Card.Body>
-          <Card.Title className="FFC-title">Animals</Card.Title>
-        </Card.Body>
-      </Card>
+        <Card>
+          <Card.Img
+            className="FFC-card"
+            variant="top"
+            src={`https://source.unsplash.com/200x200/?animal&${Math.random()}`}
+          />
+          <Card.Body>
+            <Card.Title className="FFC-title">Animals</Card.Title>
+          </Card.Body>
+        </Card>
 
-      <Card>
-        <Card.Img
-          className="FFC-card"
-          variant="top"
-          src={`https://source.unsplash.com/200x200/?household&${Math.random()}`}
-        />
-        <Card.Body>
-          <Card.Title className="FFC-title">Household Items</Card.Title>
-        </Card.Body>
-      </Card>
+        <Card>
+          <Card.Img
+            className="FFC-card"
+            variant="top"
+            src={`https://source.unsplash.com/200x200/?household&${Math.random()}`}
+          />
+          <Card.Body>
+            <Card.Title className="FFC-title">Household Items</Card.Title>
+          </Card.Body>
+        </Card>
+      </div>
+      <div className="button-container">
+        <Button variant="primary" onClick={handleGoBack}>Go Back</Button>
+      </div>
     </div>
   );
 };
