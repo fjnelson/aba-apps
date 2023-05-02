e57a5f79bb0c3baa9fed01572b9962659a8a0383
fjnelson <97076779+fjnelson@users.noreply.github.com>
2023-05-01 19:39:50 -0700

    attempting to add shimmers

--- src/components/FeatureFunctionClass/FeatureFunctionClass.js
+++ src/components/FeatureFunctionClass/FeatureFunctionClass.js
@@ -2,26 +2,42 @@ import React from "react";
 import { Card } from "react-bootstrap";
 import "../../App.css";
 
+const Shimmer = () => (
+  <div className="shimmer-wrapper">
+    <div className="shimmer"></div>
+  </div>
+);
+
 const CardComponent = () => {
+  const [loading, setLoading] = React.useState(true);
+
+  const handleImageLoad = () => {
+    setLoading(false);
+  };
 
   return (
     <div>
       <div className="FFC-container">
         <Card>
+          {loading && <Shimmer />}
           <Card.Img
             className="FFC-card"
             variant="top"
             src={`https://source.unsplash.com/200x200/?vehicle&${Math.random()}`}
+            onLoad={handleImageLoad}
           />
           <Card.Body>
             <Card.Title className="FFC-title">Vehicles</Card.Title>
           </Card.Body>
         </Card>
+
         <Card>
+          {loading && <Shimmer />}
           <Card.Img
             className="FFC-card"
             variant="top"
             src={`https://source.unsplash.com/200x200/?furniture&${Math.random()}`}
+            onLoad={handleImageLoad}
           />
           <Card.Body>
             <Card.Title className="FFC-title">Furniture</Card.Title>
@@ -29,10 +45,12 @@ const CardComponent = () => {
         </Card>
 
         <Card>
+          {loading && <Shimmer />}
           <Card.Img
             className="FFC-card"
             variant="top"
             src={`https://source.unsplash.com/200x200/?clothing&${Math.random()}`}
+            onLoad={handleImageLoad}
           />
           <Card.Body>
             <Card.Title className="FFC-title">Clothing</Card.Title>
@@ -40,10 +58,12 @@ const CardComponent = () => {
         </Card>
 
         <Card>
+          {loading && <Shimmer />}
           <Card.Img
             className="FFC-card"
             variant="top"
             src={`https://source.unsplash.com/200x200/?Food&${Math.random()}`}
+            onLoad={handleImageLoad}
           />
           <Card.Body>
             <Card.Title className="FFC-title">Food</Card.Title>
@@ -51,10 +71,12 @@ const CardComponent = () => {
         </Card>
 
         <Card>
+          {loading && <Shimmer />}
           <Card.Img
             className="FFC-card"
             variant="top"
             src={`https://source.unsplash.com/200x200/?animal&${Math.random()}`}
+            onLoad={handleImageLoad}
           />
           <Card.Body>
             <Card.Title className="FFC-title">Animals</Card.Title>
@@ -62,10 +84,12 @@ const CardComponent = () => {
         </Card>
 
         <Card>
+          {loading && <Shimmer />}
           <Card.Img
             className="FFC-card"
             variant="top"
             src={`https://source.unsplash.com/200x200/?household&${Math.random()}`}
+            onLoad={handleImageLoad}
           />
           <Card.Body>
             <Card.Title className="FFC-title">Household Items</Card.Title>
