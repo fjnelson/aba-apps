7951e029bc558a6970d78f7e177b8a2818225ada
fjnelson <97076779+fjnelson@users.noreply.github.com>
2023-06-03 08:38:56 -0700

    adding zonesofregulation to cards

---
+++ src/components/ZonesofRegulation/Regulation.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import "../../App.css";
+import blueZone from "./images/bluezone.JPG";
+import greenZone from "./images/greenzone.JPG";
+import yellowZone from "./images/yellowzone.JPG";
+import redZone from "./images/redzone.JPG";
+
+
+  
+const CardComponent = () => (
+    <div>
+      <div className="FFC-container">
+        <Card>
+          <Card.Img
+            className="FFC-card"
+            variant="top"
+            src={blueZone}
+          />
+        </Card>
+  
+        <Card>
+          <Card.Img
+            className="FFC-card"
+            variant="top"
+            src={greenZone}
+          />
+        </Card>
+  
+        <Card>
+          <Card.Img
+            className="FFC-card"
+            variant="top"
+            src={yellowZone}
+          />
+        </Card>
+  
+        <Card>
+          <Card.Img
+            className="FFC-card"
+            variant="top"
+            src={redZone}
+          />
+        </Card>
+      </div>
+    </div>
+  );
+  
+  export default CardComponent;
